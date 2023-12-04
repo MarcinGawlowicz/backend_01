d71243806940af2723e74d4268aaf383d3bb38ee
MarcinGawlowicz <MarcinGawlowicz@noreply.github.com>
2023-12-04 19:44:25 +0100

    django first project

--- hello/views.py
+++ hello/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import HttpResponse
 
 def hello_view(request):
     return HttpResponse("hello world")
+
+
+def hello_eva(request):
+    return HttpResponse("Hello, Eva!")
+
+
+def hello_adam(request):
+    return HttpResponse("Hello, Adam!")
+
+
+def hello_name(request, name):
+    return HttpResponse(f"Hello, {name.title()}!")
